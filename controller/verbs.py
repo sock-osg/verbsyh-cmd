d964a93e311af18d5583f9dc887d63fdf6773887
Samuel Quintana <sock.sqt@gmail.com>
2016-02-01 21:17:33 -0600

    The files not used were deleted and the files was renamed

--- controller/verbs.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/verbs/get_verbs/user_id/<int:user_id>", methods = ["GET"])
-def getVerbs(user_id = None):
-    return "Verbos para el usuario : " + user_id
-
-if __name__ == "__main__":
-    app.run(host = "0.0.0.0", debug = True)
